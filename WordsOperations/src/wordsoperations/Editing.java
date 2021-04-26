44b5f9dda0e6a2c35f2fd816e2f2358b01cb3e54
mati <mati@DESKTOP-OLQJ7TQ>
2021-04-26 13:56:52 +0200

    IdexOf = Buscador

---
+++ WordsOperations/src/wordsoperations/Editing.java
@@ -0,0 +1,25 @@
+package wordsoperations;
+
+public class Editing {
+
+	public static void subEditing(String wordtoEdit) {
+		                //buscador
+	  Operations.print("Substring from 3: ");
+	  Operations.print("wordtoEdit.substring(3)");
+	  Operations.linebreak();
+	  
+	  Operations.print("Substring from 3 to 6: ");
+	  Operations.print("wordtoEdit.substring(3, 6)");
+	  Operations.linebreak();
+	  
+	  Operations.print("Position @: ");
+	  Operations.print("wordtoEdit.idexOf(@)");
+	  Operations.linebreak();
+	  
+	  Operations.print("Domain: ");
+	  Operations.print("wordtoEdit.substring(wordtoEdit.idexOf(@ + 1))");
+	  Operations.linebreak();
+
+	}
+
+}
