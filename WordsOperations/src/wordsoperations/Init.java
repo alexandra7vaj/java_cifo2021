e587b923dc450b3098594bd1d4d69ff22551b38f
mati <mati@DESKTOP-OLQJ7TQ>
2021-04-28 13:29:42 +0200

    Teatcher's work

--- WordsOperations/src/wordsoperations/Init.java
+++ WordsOperations/src/wordsoperations/Init.java
@@ -7,27 +7,25 @@ public class Init {
   public static void main(String[] args) {
 
 		
-		Scanner reader = new Scanner (System.in);
-		System.out.println("Write some string ...");
-		String word = reader.nextLine();
-		
-		//Operations.print(word);
-		//Operations.linebreak();
-		//Operations.printString(word);
-		
-		//Operations.printStringH(word);
-		//Operations.printStringV(word);
-		//Operations.printStringHDash(word);
-		//Operations.printStringDiagonal(word);
-		//Operations.printStringBackWards(word);
-		
-		//Operations.operationsString(word);
-		Editing.subEditing (word);
-		
-		EmailManagment.createAccount(word);
-		
-		
-		
+		try (Scanner reader = new Scanner (System.in)) {
+			System.out.println("Write some string ...");
+			String word = reader.nextLine();
+			
+			//Operations.print(word);
+			//Operations.linebreak();
+			//Operations.printString(word);
+			
+			//Operations.printStringH(word);
+			//Operations.printStringV(word);
+			//Operations.printStringHDash(word);
+			//Operations.printStringDiagonal(word);
+			//Operations.printStringBackWards(word);
+			
+			//Operations.operationsString(word);
+			Editing.subEditing (word);
+			
+			EmailManagment.createAccount(word);
+		}
 		System.out.println("\nBye bye my friend...");
 		
 		
